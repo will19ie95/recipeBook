b9c970fb461a104a2b3ae5faa530c725bfa06769
Yong Lei <will19ie95@gmail.com>
2017-06-18 14:20:39 -0400

    implementing template driven form in shopping list and edit

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,19 +10,15 @@ import { ShoppingListService } from '../shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit {
 
-@ViewChild('nameInput') nameInputRef : ElementRef;
-@ViewChild('amountInput') amountInputRef : ElementRef;
-// @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAddItem() {
-    const name = this.nameInputRef.nativeElement.value;
-    const amount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(name, amount);
+  onAddItem(form: NgForm) {
+    const value = form.value
+    const newIngredient = new Ingredient(value.name, value.amount);
     console.log("adding ingredient",newIngredient)
     this.shoppingListService.addIngredient(newIngredient);
   }
